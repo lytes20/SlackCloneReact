723a0195da63cb2fa0ea9334e17431992f685a68
Gideon Bamuleseyo <gideonbamuleseyo@gmail.com>
2019-10-21 11:15:47 +0300

    Add travis

--- src/components/ContactsDisplay.jsx
+++ src/components/ContactsDisplay.jsx
@@ -5,6 +5,20 @@ import { Contacts } from "../data";
 const ContactsDisplay = () => {
   return (
     <div className="contacts-container">
+      <h1>Company Name</h1>
+      <form>
+        <input type="text" placeholder="jump to"/>
+      </form>
+      {/* Channels list */}
+      <h1>Channels</h1>
+      <ul>
+        {Contacts.map(contact => (
+          <li key={contact.id}># {contact.name}</li>
+        ))}
+      </ul>
+
+      {/* Contacts list */}
+      <h1>Direct messages</h1>
       <ul>
         {Contacts.map(contact => (
           <li key={contact.id}>{contact.name}</li>
